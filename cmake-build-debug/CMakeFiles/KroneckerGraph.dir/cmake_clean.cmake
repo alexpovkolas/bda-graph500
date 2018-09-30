fcdbf9eb1361ae0f7ded758a194e2034d96c5c0d
Aliaksandr Paukalas <alexander.povkolas@gmail.com>
2018-09-30 16:32:04 +0300

    Add generator implementation from graph500 test

---
+++ cmake-build-debug/CMakeFiles/KroneckerGraph.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KroneckerGraph.dir/main.cpp.o"
+  "KroneckerGraph.pdb"
+  "KroneckerGraph"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KroneckerGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
