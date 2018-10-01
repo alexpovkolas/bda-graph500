abffbb525e5028d276cebed8022b546098b0825b
Aliaksandr Paukalas <alexander.povkolas@gmail.com>
2018-10-01 04:18:36 +0300

    Add barabasi-albert graph tests

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@
 #include <random>
 #include <map>
 #include <fstream>
+#include <string>
 
 
 
 #define EDGE_FACTOR 16
 #define SCALE 12
-#define TESTS_AMOUNT 1000
+#define TESTS_AMOUNT 10
 #define SEED 777
 #define DIRECTED true
 
@@ -51,6 +52,24 @@ void generate_igraph(igraph_t *igraph, int scale, uint64_t seed1, int seed2) {
     igraph_create(igraph, &edges_vector, 0, IGRAPH_DIRECTED);
 }
 
+void generate_barabasi_albert_graph(igraph_t *igraph, int n, int m) {
+
+    igraph_t g;
+    igraph_bool_t simple;
+
+    igraph_barabasi_game(/* graph=    */ igraph,
+            /* n=        */ n,
+            /* power=    */ 1.0,
+            /* m=        */ m,
+            /* outseq=   */ 0,
+            /* outpref=  */ 0,
+            /* A=        */ 1.0,
+            /* directed= */ IGRAPH_DIRECTED,
+            /* algo=     */ IGRAPH_BARABASI_BAG,
+            /* start_from= */ 0);
+}
+
+
 igraph_integer_t diameter(igraph_t *igraph){
     igraph_integer_t diameter;
     igraph_diameter(igraph, &diameter, 0, 0, 0, DIRECTED ? IGRAPH_DIRECTED : IGRAPH_UNDIRECTED, 1);
@@ -78,8 +97,25 @@ void hist(igraph_t *igraph, vector<int> &frequencies){
 }
 
 
-int main(int argc, char** argv) {
-    aml_init(&argc,&argv); //includes MPI_Init inside
+void print_hist(const vector<int> freq, int nvert, int test_amount, const char* file_name, int block = 100) {
+    ofstream output(file_name, ios_base::out | ios_base::trunc);
+    int i = 0;
+    int acc = 0;
+    for(auto it = freq.cbegin(); it != freq.cend(); ++it) {
+
+        if (i < block) {
+            acc += *it;
+            ++i;
+        } else {
+            output << acc / (double) test_amount / (double) nvert << "\n";
+            acc = *it;
+            i = 1;
+        }
+    }
+
+}
+
+void test_kronecker_graph() {
     setup_globals();
 
     std::mt19937 rng;
@@ -121,29 +157,64 @@ int main(int argc, char** argv) {
     printf("Avg distance of the graph: %f\n",
            avg_distance);
 
-    ofstream output("output", ios_base::out | ios_base::trunc);
-
-
-    int block = 100;
-    int i = 0;
-    int acc = 0;
-    for(auto it = histogram.cbegin(); it != histogram.cend(); ++it) {
-
-        if (i < block) {
-            acc += *it;
-            ++i;
-        } else {
-            output << acc / (double) TESTS_AMOUNT / nvert << "\n";
-            acc = *it;
-            i = 1;
-        }
-    }
+    print_hist(histogram, nvert, TESTS_AMOUNT, "output_kronecker");
 
     double tests_stop = MPI_Wtime();
     double tests_time = tests_stop - tests_start;
 
     fprintf(stderr, "running time:               %f s\n", tests_time);
+}
 
+void test_barabasi_albert_graph() {
+
+    double tests_start = MPI_Wtime();
+
+    int test_amount = 2;
+    int diameter_sum = 0;
+    double distance_sum = 0;
+
+    int n = 1000000;
+    int m = 16;
+    vector<int> histogram(n, 0);
+    for (int k = 0; k < test_amount; ++k) {
+
+        igraph_t graph;
+
+        generate_barabasi_albert_graph(&graph, n, m);
+
+
+        diameter_sum += diameter(&graph);
+        distance_sum += avg_distance(&graph);
+        hist(&graph, histogram);
+
+
+        igraph_destroy(&graph);
+    }
+
+    double avg_diameter = diameter_sum  / (double)test_amount;
+    double avg_distance = distance_sum  / (double)test_amount;
+
+    printf("Avg diameter of the graph: %f\n",
+           avg_diameter);
+
+    printf("Avg distance of the graph: %f\n",
+           avg_distance);
+
+    print_hist(histogram, n, test_amount, "output_barabasi_albert", 10);
+
+    double tests_stop = MPI_Wtime();
+    double tests_time = tests_stop - tests_start;
+
+    fprintf(stderr, "running time:               %f s\n", tests_time);
+}
+
+
+
+int main(int argc, char** argv) {
+    aml_init(&argc,&argv); //includes MPI_Init inside
+
+    //test_kronecker_graph();
+    test_barabasi_albert_graph();
 
     return 0;
 }
